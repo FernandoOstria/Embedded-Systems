139188db47f2e0e62c5ee5bba88f425362b7943c
KevinInzu <45952314+KevinInzu@users.noreply.github.com>
2022-02-15 17:39:42 -0800

    Update Question3.c

--- Lab1/Code/Question3.c
+++ Lab1/Code/Question3.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
+#include <limits.h> //CHAR_BIT is defined in limits.h
 
-unsigned int setbits(unsigned, int, int, unsigned);
-
-unsigned int setbits(unsigned x, int p, int n, unsigned y)
+unsigned setbits(unsigned x, int p, int n, unsigned y)
 {
-  int shift = p - n + 4;
-  unsigned mask = (1 << n) - 1;
-  return (x & ~(mask << shift)) | ((y & mask) << shift);
+        
+   int shift = p - n + 4;
+    unsigned mask = (1 << n) - 1;
+    return (x & ~(mask << p)) | ((y & mask) << p);
+
 }
+
 
 int main(void)
 {
-  unsigned x = 0XF8FF;
-  unsigned y = 0XF0A2;
-  int p = 7;
-  int n = 4;
-  
-  printf("%x\n", setbits(x, p, n, y));
-  
-  return 0;
+        unsigned x = 0xF8FF;
+        unsigned y = 0XF0A2;
+
+        int p =7;
+        int n =4;
+
+
+        printf("%x\n",setbits(x,p,n,y));
 }

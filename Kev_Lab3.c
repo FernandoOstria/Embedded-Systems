186051890f4fe431ff95261b36912b2c7a1c928d
KevinInzu <45952314+KevinInzu@users.noreply.github.com>
2022-03-06 16:28:04 -0800

    Update Kev_Lab3.c

--- Kev_Lab3.c
+++ Kev_Lab3.c
@@ -6,19 +6,23 @@ void main(void)
 {
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;        // stop watchdog timer
 
-    // Port 2 controls the RGB LED (LED2)
+    /*
+
+     Port 2 controls the RGB LED (LED2), modifying only 7 bits(111) because the led has 7 possible combinations
+         using an AND mask to clear the right 3 most bits (set to zero)
+         Using an OR mask to set the bit to 1, Thus making it an output
+
+    */
     P2->SEL1 &= ~0x07;
     P2->SEL0 &= ~0x07;
-
-    P2->DIR |= 0x07; 
+    P2->DIR |= 0x07;
 
     int j = 0;
-
     for (j= 0; j <= 7; j++)
     {
-        _delay_cycles(sec);
-        P2->OUT = j & 7; //mask i the counter by 7, since the highest the LED can go before repeating itself is 7. Thus we keep the right most 3 bits
+        P2->OUT = j & 7; //mask j the counter by 7, since the highest the LED can go before repeating itself is 7. Thus we keep the right most 3 bits
         printf("%hd \n", P2->OUT );
+        _delay_cycles(sec);
     }
 
     while(1)

dc0a21456944a09895265e84dc0e2f88d972d49c
KevinInzu <45952314+KevinInzu@users.noreply.github.com>
2022-04-04 18:56:19 -0700

    Working

--- Lab5/kev_lab5.c
+++ Lab5/kev_lab5.c
@@ -11,52 +11,82 @@
 #include <stdarg.h>
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
 
+const eUSCI_UART_Config uartConfig =
+{
+        EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
+        1,                                        // BRDIV = 1
+        10,                                       // UCxBRF = 10
+        0,                                       // UCxBRS = 0
+        EUSCI_A_UART_NO_PARITY,                  // No Parity
+        EUSCI_A_UART_LSB_FIRST,                  // LSB First
+        EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
+        EUSCI_A_UART_MODE,                       // UART mode
+        EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION  // Oversampling
+};
+
+inline void uart_println(const char* str, ...)
+{
+  static char print_buffer[256];
+  va_list lst;
+  va_start(lst, str);
+  vsnprintf(print_buffer, 256, str, lst);
+  str = print_buffer;
+  while (*str)
+  {
+    while (!(UCA0IFG & EUSCI_A_IFG_TXIFG));
+    UCA0TXBUF = *str;
+    str++;
+  }
+  while (!(UCA0IFG & EUSCI_A_IFG_TXIFG));
+  UCA0TXBUF = '\r';
+  while (!(UCA0IFG & EUSCI_A_IFG_TXIFG));
+  UCA0TXBUF = '\n';
+}
+
 void main(void)
 {
-    char text[50]= "This is a test";
+    /* Halting WDT  */
+        MAP_WDT_A_holdTimer();
 
-    const eUSCI_UART_Config uartConfig =
-    {
-     EUSCI_A_UART_CLOCKSOURCE_SMCLK,
-     78,
-     2,
-     0,
-     EUSCI_A_UART_NO_PARITY,
-     EUSCI_A_UART_LSB_FIRST,
-     EUSCI_A_UART_ONE_STOP_BIT,
-     EUSCI_A_UART_MODE,
-     EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION,
-    };
+        /* Setting DCO to 12MHz */
+           CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_3); // to select 3 MHZ
 
-    WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;        // stop watchdog timer
+        /* Selecting P1.2 and P1.3 in UART mode */
+        MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,
+                GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
 
-    UART_initModule(EUSCI_A0_BASE,&uartConfig);
-    UART_transmitData(EUSCI_A0_BASE,text[1]);
+             /* Configuring UART Module */
+        MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
 
-    // Bottom works
-    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, 0x012);
-    GPIO_setAsOutputPin(GPIO_PORT_P2,0x07);
+        /* Enable UART module */
+        MAP_UART_enableModule(EUSCI_A0_BASE);
+
+        //Setting GPIO's
+        GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P1, 0x012);
+        GPIO_setAsOutputPin(GPIO_PORT_P2,0x07);
 
     while(1)
     {
+        /* We want to read the 2nd bit to check whether button1 is an active high, then read 5th bit to check whether the button2 is an active high on Port 1.
+           Stored input register */
         int Switch1_Pressed = !(GPIO_getInputPinValue(GPIO_PORT_P1,GPIO_PIN1));
         int Switch2_Pressed = !(GPIO_getInputPinValue(GPIO_PORT_P1,GPIO_PIN4));
 
 
         if (Switch1_Pressed && Switch2_Pressed)
         {
-            printf("Both Pressed \n");
+            uart_println("Both Pressed \n");
             GPIO_setOutputHighOnPin(GPIO_PORT_P2, 0x07);
         }
         else if(Switch1_Pressed)
         {
-            printf("Switch1 Pressed \n");
+            uart_println("Switch1 Pressed \n");
             GPIO_setOutputHighOnPin(GPIO_PORT_P2, 0x04);
 
         }
         else if (Switch2_Pressed)
         {
-            printf("Switch2 Pressed \n");
+            uart_println("Switch2 Pressed \n");
             GPIO_setOutputHighOnPin(GPIO_PORT_P2, 0x01);
 
         }
@@ -68,3 +98,7 @@ void main(void)
 
 
 }
+
+
+
+
